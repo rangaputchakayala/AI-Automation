baf71a76828ae65ee039a38d454dc81ad636141f
Raghu-Satti <rsatti@alphaprotemps.com>
2026-07-23 17:50:01 +0530

    Change password input to empty string
    Updated password input to be empty for login.

--- Login_page.py
+++ Login_page.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     browser = login("https://qa-agent-admin-ui.alphaatlus.com/")
     browser.open()
     browser.fill_email_and_submit("rsatti@alphaprotemps.com")
-    browser.fill_password_and_sign_in("SRNR@27265")
+    browser.fill_password_and_sign_in("")
     browser.stay_open()
